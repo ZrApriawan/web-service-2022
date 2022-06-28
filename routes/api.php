ef9556ef0e66f2d5e0558075fbaee24a75687a3d
ZrApriawam <ZeraApriawan@gmail.com>
2022-06-28 14:24:05 +0800

    Pembuatan Auth

--- routes/api.php
+++ routes/api.php
@@ -33,3 +33,14 @@ Route::group(['prefix'=> 'v1'], function() {
 
     Route::delete('customer/{id}', [CustomerController::class, 'destroy']);
 });
+
+
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('refresh', [\App\Http\Controllers\AuthController::class, 'refresh']);
+    Route::post('me', [\App\Http\Controllers\AuthController::class, 'me']);
+
+});
